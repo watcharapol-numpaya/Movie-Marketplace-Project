3dbde01145945ef20355b7f53c4c7bea8612e992
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-09-09 13:12:06 +0700

    [ADD] add redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./component/HomePage";
+
+
+const App = () => {
  
-const  App = ()=> {
-  const [count, setCount] = useState(0)
-
   return (
-<div className=' '></div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/home" element={<HomePage/>}></Route>
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./app/store";
+import { store } from  './storage/store.js'
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
